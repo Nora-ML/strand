a8568d233c96b67c146d1dd6ed107fa1359c78a1
Nora-ML <86887904+Nora-ML@users.noreply.github.com>
2022-08-29 14:57:04 +0200

    setup +hero

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./hero.styles.scss"
+
+const Hero = () => {
+    return (
+        <section class="hero is-fullheight hero-image">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="hero-title">
+             
+              </h1>
+              <div className="shop-now-btn">
+                  <button className="button is-black" id='shop-now'>
+                      Shop Now
+                  </button>
+              </div>
+            </div>
+          </div>
+        </section>
+    )
+}
+export default Hero;
